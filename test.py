73673fde6a67ad0267c0f60a2c8117a00ef55ebc
ted.song <ted.song@daumkakao.com>
2017-08-15 20:24:21 +0900

    mylib.py

--- test.py
+++ test.py
@@ -1,10 +1,22 @@
 import math
+import unittest
+from mylib import *
 
 a = 1
 b = 2
 c = a + b
 
-print(c)
-
 n = math.sqrt(9)
-print(n)
+
+def sum(a, b):
+    return a + b
+
+class Test(unittest.TestCase):
+    def testAdd(self):
+        self.assertEqual(c, 3)
+
+    def testSum(self):
+        self.assertEqual(sum(1, 2), 3)
+
+    def testSubtract(self):
+        self.assertEqual(substract(2, 1), 1)
